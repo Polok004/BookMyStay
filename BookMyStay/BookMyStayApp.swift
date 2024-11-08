a4dbff05da6dbc404513c0550e51e8c4a0f49c82
Polok004 <polok2007004@stud.kuet.ac.bd>
2024-11-08 15:25:28 +0600

    Initial Commit

---
+++ BookMyStay/BookMyStayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyStayApp.swift
+//  BookMyStay
+//
+//  Created by Shoumik Barman Polok on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BookMyStayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookMyStayTests/BookMyStayTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookMyStayTests.swift
+//  BookMyStayTests
+//
+//  Created by Shoumik Barman Polok on 8/11/24.
+//
+
+import Testing
+@testable import BookMyStay
+
+struct BookMyStayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
